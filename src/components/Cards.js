f29b5a508498402eff0ea576859ac4672c67668d
Ketan Sahu <rorozoro9888@gmail.com>
2023-06-10 21:07:29 +0530

    working on cards props

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,21 +1,21 @@
 import React from "react";
-import Zaphares from "../Images/zaphares.png"
+/* import Zaphares from "../Images/zaphares.png" */
 import Star from "../Images/Star 1.png"
 
-export default function Cards() {
+export default function Cards(props) {
     return (
         <section className="card-container">
             <div className="card-badge">SOLD OUT</div>
-            <img src={Zaphares} 
+            <img src={`../Images/${props.img}`} 
             alt="Zaphares" className="card-img"/>
             <div className="card-info">
                 <div className="card-stats">
                     <img src={Star} alt="star" className="star" />
-                    <p></p>
-                    <p className="grey">(6) · USA</p>
+                    <p>{props.rating}</p>
+                    <p className="grey">({props.reviewCount}) • {props.location}</p>
                 </div>
-                <p>Life lessons with Katie Zafares</p>
-                <p><b>From $136</b> / person</p>
+                <p>{props.title}</p>
+                <p><b>From ${props.price}</b> / person</p>
             </div>
         </section>
     )
