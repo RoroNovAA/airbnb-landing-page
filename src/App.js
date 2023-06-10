4c67db8f59891d050b6b85a43fae55e442bf6b3b
Ketan Sahu <rorozoro9888@gmail.com>
2023-06-10 16:55:48 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cards from "./components/Cards"
+import data from "./data"
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Cards 
+           key={item.id}
+           img={item.coverImg}
+           rating={item.stats.rating}
+           reviewCount={item.stats.reviewCount}
+           location={item.location}
+           title={item.title}
+           price={item.price}
+           openSpots={item.openSpots}
+      />
+
+    )
+  })
   return (
     <div className="App">
         <Navbar />
-        {/* <Hero /> */}
-        <Cards />
+      {/* <Hero /> */}
+        {cards}
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,15 +4,19 @@ import Star from "../Images/Star 1.png"
 
 export default function Cards() {
     return (
-        <section>
-            <img src={Zaphares} alt="Zaphares" />
-            <div>
-                <img src={Star} alt="star" />
-                <p>5.0</p>
-                <p>(6)</p>
-                <p>· USA</p>
+        <section className="card-container">
+            <div className="card-badge">SOLD OUT</div>
+            <img src={Zaphares} 
+            alt="Zaphares" className="card-img"/>
+            <div className="card-info">
+                <div className="card-stats">
+                    <img src={Star} alt="star" className="star" />
+                    <p></p>
+                    <p className="grey">(6) · USA</p>
+                </div>
+                <p>Life lessons with Katie Zafares</p>
+                <p><b>From $136</b> / person</p>
             </div>
-                
         </section>
     )
 }
